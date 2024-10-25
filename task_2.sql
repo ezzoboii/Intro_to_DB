b5914721c80208ba0038806f43a9ed43f52f36b9
Ezrah <ezrahpietersen309@gmail.com>
2024-10-25 06:20:52 -0700

    changes made

--- task_2.sql
+++ task_2.sql
@@ -58,3 +58,5 @@ customer_name VARCHAR(215)
 email VARCHAR(215)
 
 Orders", "FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+
+Order_Details", "quantity DOUBLE", "FOREIGN KEY (order_id) REFERENCES Orders(order_id)", "FOREIGN KEY (book_id) REFERENCES Books(book_id)
